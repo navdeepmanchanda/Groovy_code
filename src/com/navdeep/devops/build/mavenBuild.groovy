5e886e01767c0caa847a5d1eade1ef8e9850b131
navdeepmanchanda <navdeepmanchanda@gmail.com>
2019-04-14 15:04:50 +0530

    Update mavenBuild.groovy

--- src/com/navdeep/devops/build/mavenBuild.groovy
+++ src/com/navdeep/devops/build/mavenBuild.groovy
@@ -7,7 +7,9 @@ package com.navdeep.devops.build
 def compile(String POM_FILE, String MVN_GOALS)
 {
    try {
-      sh "mvn -f $POM_FILE $MVN_GOALS"
+      withEnv(["PATH+JAVA=${tool 'JDK8'}/bin","PATH+MVN=${tool 'MAVEN3'}/bin"]) {
+        sh "mvn -f $POM_FILE $MVN_GOALS"
+      }
    }
    catch (Exception e) {
      print "\u001B[41mERROR: mvn failed while running goals $MVN_GOALS, exiting..."
